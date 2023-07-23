94af4bc6fc468a6f0a8ec80278c238d9648812a6
Omid Mokhtari <60341517+amisteromid@users.noreply.github.com>
2023-07-23 21:58:46 +0200

    Add files via upload

---
+++ rosalind_conv.py
@@ -0,0 +1,28 @@
+from decimal import Decimal
+
+def largest_multiplicity(s, t):
+    # Calculate the Minkowski setserence from both sets.
+    sets = {}
+    for i in s:
+        for j in t:
+            d = i - j
+            if d in sets:
+                sets[d] += 1
+            else:
+                sets[d] = 1
+    
+    # Find the largest multiplicity and return it.
+    largest = max((v, k) for k ,v in sets.items())
+    
+    return largest
+    
+    
+def main():
+    with open('rosalind_conv.txt', 'r') as infile:
+        s, t = [[Decimal(x) for x in line.split()] for line in infile.read().strip().split('\n')]      
+        
+    print('\n'.join(map(str, largest_multiplicity(s, t))))
+    
+
+if __name__ == '__main__':
+    main()
